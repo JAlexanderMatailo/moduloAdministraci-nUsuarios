23706dfdfc6c692546d93aed4d739ccf1e7e7e3e
JAlexanderMatailo <52136249+JAlexanderMatailo@users.noreply.github.com>
2023-09-12 21:50:37 -0500

    ajuste en clase program

--- moduloAdministraciónUsuarios/Program.cs
+++ moduloAdministraciónUsuarios/Program.cs
@@ -1,9 +1,11 @@
 using dbPrueba;
 using Microsoft.EntityFrameworkCore;
+using moduloAdministraciónUsuarios.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUsuario, UsuarioService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,11 +13,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+#region Conexion a Base de Datos
 builder.Services.AddDbContext<pruebaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PruebaConnection"))
 );
+#endregion
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "API",
+                      builder =>
+                      {
+                          builder.WithHeaders("*");
+                          builder.WithOrigins("*");
+                          builder.WithMethods("*");
 
+                      });
+});
 
 var app = builder.Build();
 
@@ -38,6 +52,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("API");
 
 app.MapControllers();
 
